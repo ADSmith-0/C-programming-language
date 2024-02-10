0f9d89d9b0559392ad994a0c27f65230a41b3f95
adam smith <adam00199382818@gmail.com>
2024-02-10 15:16:52 +0000

    added celsius to fahrenheit table

--- src/CelsiusToFahrenheit.c
+++ src/CelsiusToFahrenheit.c
@@ -4,12 +4,21 @@ int main() {
   int upper = 300;
   int step = 20;
 
-  int celsius;
-  int fahrenheit = lower;
+  float celsius;
+  float fahrenheit = lower;
+  printf("Fahrenheit Celsius\n");
   while (fahrenheit <= upper) {
-    celsius = 5 * (fahrenheit - 32) / 9;
-    printf("%d\t%d\n", fahrenheit, celsius);
+    celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+    printf("%10.0f %7.1f\n", fahrenheit, celsius);
     fahrenheit = fahrenheit + step;
+  }
+  printf("\n");
+  printf("Celsius Fahrenheit\n");
+  celsius = 0;
+  while (celsius <= upper) {
+    fahrenheit = (9.0 / 5.0) * (celsius + 32.0);
+    printf("%7.0f %10.1f\n", celsius, fahrenheit);
+    celsius = celsius + step;
   }
   return 0;
 }
