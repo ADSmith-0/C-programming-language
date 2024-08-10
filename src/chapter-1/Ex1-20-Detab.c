4a823d64364b0ed50a40d01d2d983660634ed14e
adam smith <adam00199382818@gmail.com>
2024-08-10 16:59:34 +0100

    added Ex1-20

---
+++ src/chapter-1/Ex1-20-Detab.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define MAX_LENGTH 1000
+
+int getLine(char line[], int limit);
+void clear(char line[], int limit);
+int detab(char to[], char from[]);
+
+int main() {
+  int length = 0;
+  int detabbedLength = 0;
+
+  char line[MAX_LENGTH];
+  char detabbedLine[MAX_LENGTH];
+
+  while ((length = getLine(line, MAX_LENGTH)) > 0) {
+    clear(detabbedLine, MAX_LENGTH);
+    detabbedLength = detab(detabbedLine, line);
+    printf("original length: %4d, original: %s\ndetabbed length: %4d, "
+           "detabbed: %s\n\n",
+           length, line, detabbedLength, detabbedLine);
+  }
+
+  return 0;
+}
+
+int getLine(char line[], int limit) {
+  char character;
+  int i = 0;
+
+  while (i < limit - 1 && (character = getchar()) != EOF && character != '\n') {
+    line[i] = character;
+    i++;
+  }
+
+  line[i] = '\0';
+  return i;
+}
+
+void clear(char line[], int limit) {
+  int i = 0;
+
+  while (i < limit - 1) {
+    line[i] = '\0';
+    i++;
+  }
+}
+
+int detab(char to[], char from[]) {
+  int toIndex = 0;
+  int fromIndex = 0;
+
+  while (from[fromIndex] != '\0') {
+    if (from[fromIndex] == '\t') {
+      to[toIndex++] = ' ';
+      to[toIndex] = ' ';
+    } else {
+      to[toIndex] = from[fromIndex];
+    }
+
+    toIndex++;
+    fromIndex++;
+  }
+
+  return toIndex;
+}
